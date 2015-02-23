bda208aa002ffdebc8bdd4af4585c6b295adda75
Brandon Houghton <brandon.houghton@designory.com>
2015-02-23 17:19:59 -0600

    Add build task and basic image tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,17 @@
 var gulp = require('gulp');
 var loadPlugins = require('gulp-load-plugins');
 var del = require('del');
+var path = require('path');
 var fs = require('fs');
+var ms = require('merge-stream');
 var sequence = require('run-sequence');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var plugins = loadPlugins({
-        rename: {
-            'gulp-svg-symbols': 'symbols'
-        }
-    });
+    rename: {
+        'gulp-svg-symbols': 'symbols'
+    }
+});
 
 
 // site paths
@@ -24,7 +26,8 @@ var paths = {
         scripts: {
             root: 'src/assets/scripts/',
             head: 'src/assets/scripts/head/**/*.js',
-            body: ['src/assets/scripts/body/global/**/*.js', 'src/assets/scripts/body/modules/**/*.js', 'src/assets/scripts/body/**/*.js']
+            body: ['src/assets/scripts/body/global/**/*.js', 'src/assets/scripts/body/modules/**/*.js', 'src/assets/scripts/body/**/*.js'],
+            build: ['src/assets/scripts/*.js']
         },
         image: ['src/assets/images/**/*', '!src/assets/images/svg/symbols.svg'],
         css: 'src/assets/css/scss/**/*.scss',
@@ -33,7 +36,7 @@ var paths = {
     dist: {
         root: 'dist',
         scripts: ['dist/assets/scripts/'],
-        image: ['dist/assets/images/'],
+        image: 'dist/assets/images/',
         css: 'dist/assets/css',
         svg: ['src/assets/images/svg/']
     }
@@ -41,75 +44,109 @@ var paths = {
 
 // browsers for autoprefixer
 var browsers = [
-        'ie >= 9',
-        'ie_mob >= 9',
-        'ff >= 35',
-        'chrome >= 35',
-        'safari >= 7',
-        'opera >= 25',
-        'ios >= 7',
-        'android >= 4.4',
-        'bb >= 10'
-    ];
+    'ie >= 9',
+    'ie_mob >= 9',
+    'ff >= 35',
+    'chrome >= 35',
+    'safari >= 7',
+    'opera >= 25',
+    'ios >= 7',
+    'android >= 4.4',
+    'bb >= 10'
+];
 
+var svgConfig = {
+    mode: {
+        symbol: true
+    }
+};
+
+
+//compile scripts from head and body directories
 gulp.task('scripts', function () {
-
-    gulp.src(paths.src.scripts.head)
+    var headScripts = gulp.src(paths.src.scripts.head)
         .pipe(plugins.sourcemaps.init())
         .pipe(plugins.concat('head.js'))
         .pipe(plugins.sourcemaps.write())
         .pipe(gulp.dest(paths.src.scripts.root));
 
-    console.log(paths.src.scripts.root + 'head.js written');
-
-    gulp.src(paths.src.scripts.body)
+    var bodyScripts = gulp.src(paths.src.scripts.body)
         .pipe(plugins.sourcemaps.init())
         .pipe(plugins.concat('body.js'))
-        .pipe(plugins.sourcemaps.write())
-        .pipe(gulp.dest(paths.src.scripts.root));
+        .pipe(plugins.sourcemaps.write());
 
-    console.log(paths.src.scripts.root + 'body.js written');
+    return ms(headScripts, bodyScripts).pipe(gulp.dest(paths.src.scripts.root));
 });
 
-gulp.task('styles', function () {
-    gulp.src(paths.src.css)
-        .pipe(plugins.sourcemaps.init())
-        .pipe(plugins.sass())
-        .pipe(plugins.sourcemaps.write({
-            includeContent: false
-        }))
-        .pipe(plugins.autoprefixer(browsers))
-        .pipe(gulp.dest('src/assets/css/'))
-        .pipe(gulp.dest(paths.dist.css))
-        .pipe(plugins.sourcemaps.init({
-            loadMaps: true
-        }))
+// scripts build task - minify and place in dist
+gulp.task('build-scripts', function () {
+    return gulp.src(paths.src.scripts.build)
+        .pipe(gulp.dest('./dist/assets/scripts/'))
+        .pipe(plugins.uglify())
         .pipe(plugins.rename(function (path) {
             path.basename += ".min";
         }))
-        .pipe(plugins.minifyCss())
-        .pipe(plugins.sourcemaps.write({
-            includeContent: false
-        }))
-        .pipe(gulp.dest(paths.dist.css))
+        .pipe(gulp.dest('./dist/assets/scripts/'));
+});
+
+//compile scss and sourcemaps into main.css and appropriate files
+gulp.task('styles', function () {
+    return gulp.src(paths.src.css)
+        .pipe(plugins.sourcemaps.init())
+        .pipe(plugins.sass())
+        .pipe(plugins.autoprefixer(browsers))
+        .pipe(plugins.sourcemaps.write())
+        .pipe(gulp.dest('src/assets/css/'))
         .pipe(reload({
             stream: true
         }));
 });
 
-gulp.task('build', ['styles', 'scripts'], function () {
-    gulp.src(paths.src.html)
+//minify and place css in dist
+gulp.task('build-styles', function () {
+    return gulp.src('src/assets/css/**/*.css')
+        .pipe(gulp.dest(paths.dist.css))
+        .pipe(plugins.sourcemaps.init({ loadMaps: true }))
+        .pipe(plugins.rename(function (path) { path.basename += ".min"; }))
+        .pipe(plugins.minifyCss())
+        .pipe(plugins.sourcemaps.write({ includeContent: false }))
+        .pipe(gulp.dest(paths.dist.css));
+});
+
+//optimize images
+gulp.task('images', function () {
+    return gulp.src(paths.src.image)
+        .pipe(gulp.dest(paths.dist.image));
+});
+
+//build svg into single file
+gulp.task('sprites', function () {
+    return gulp.src(paths.src.svg)
+        .pipe(plugins.svgSprite(svgConfig))
+        .pipe(gulp.dest('src/assets/images/'))
+        .pipe(gulp.dest(paths.dist.image));
+});
+
+//build handlebars templates to html and place in dist
+gulp.task('html', function () {
+    return gulp.src(paths.src.html)
         .pipe(plugins.hb({
             data: paths.src.root + '/models/**/*.json',
             helpers: paths.src.root + '/assets/scripts/helpers/*.js',
             partials: paths.src.root + '/views/partials/**/*.hbs'
         }))
-        .pipe(plugins.rename(function (path){
+        .pipe(plugins.rename(function (path) {
             path.extname = '.html';
         }))
         .pipe(gulp.dest('./dist/'));
 });
 
+//run build specific tasks
+gulp.task('build', ['build-styles', 'build-scripts', 'html'], function () {
+    return;
+});
+
+//sync browser using browser-sync
 gulp.task('browser-sync', ['dev'], function () {
     setTimeout(function () {
         browserSync.init(null, {
@@ -119,9 +156,9 @@ gulp.task('browser-sync', ['dev'], function () {
             startPath: "/index"
         });
     }, 1000);
-
 });
 
+//dev using nodemon
 gulp.task('dev', ['clean', 'styles', 'scripts'], function (callback) {
     var called = false;
 
@@ -135,11 +172,14 @@ gulp.task('dev', ['clean', 'styles', 'scripts'], function (callback) {
     });
 });
 
+//delete dist folder
 gulp.task('clean', del.bind(null, ['dist']));
 
+//sync and watch
 gulp.task('sync', ['styles', 'scripts', 'browser-sync'], function () {
     gulp.watch(['src/assets/css/scss/**/*.scss'], ['styles']);
     gulp.watch(['src/assets/js/**/*.js'], reload);
 });
 
+//default
 gulp.task('default', ['clean', 'styles']);
