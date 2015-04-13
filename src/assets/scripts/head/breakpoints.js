da0d43f04d7770946bc0972922c29f244601b3ec
Brandon Houghton <brandon.houghton@designory.com>
2015-04-13 10:30:22 -0500

    Update to breakpoints.js to set on DOM load

--- src/assets/scripts/head/breakpoints.js
+++ src/assets/scripts/head/breakpoints.js
@@ -1,39 +1,49 @@
+/*global window,Modernizr,mzr */
+
 // set breakpoints
 mzr.breakpoints = {
-    "small"     : 767,
-    "medium"    :1024,
-    "large"     :1280,
-    "large"     :1919,
-    "xlarge"    :1920,
+  "small": 767,
+  "medium": 1024,
+  "large": 1280,
+  "xlarge": 1919,
+  "xxlarge": 1920,
+};
+
+var oldDisplayMode = mzr.breakpoints.current || '',
+  nowWindowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+function getCurrentBreakpoint() {
+  var latest;
+  var key;
+
+  for (key in mzr.breakpoints) {
+    if (mzr.breakpoints.hasOwnProperty(key)) {
+      if (key === "current") { break; }
+      if (nowWindowWidth <= mzr.breakpoints[key]) { return key; }
+      latest = key;
+    }
+  }
+
+  return latest;
 }
 
+mzr.breakpoints.current = getCurrentBreakpoint();
+
+if (mzr.breakpoints.current !== oldDisplayMode) {
+
+  // trigger event for jQuery
+  if (window.jQuery) { $(window).trigger('breakpointChange', mzr.breakpoints.current); }
+
+  // broadcast event for Angular
+  //if (window.Angular) angular.element(document).scope().$broadcast('breakpointChange', mzr.breakpoints.current);
+}
+
+//on dom load, set current breakpointgit 
+document.addEventListener("DOMContentLoaded", function () {
+  getCurrentBreakpoint();
+});
+
 // listen for window resize
-window.addEventListener('resize', function(event){
-    
-    var oldDisplayMode = mzr.breakpoints.current || '',
-        nowWindowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-
-    mzr.breakpoints.current = getCurrentBreakpoint(); 
-
-    if (mzr.breakpoints.current != oldDisplayMode) { 
-
-        // trigger event for jQuery
-        if (window.jQuery) $(window).trigger('breakpointChange', mzr.breakpoints.current);
-        
-        // broadcast event for Angular
-        //if (window.Angular) angular.element(document).scope().$broadcast('breakpointChange', mzr.breakpoints.current);
-    }
-
-    function getCurrentBreakpoint(){
-        var latest;
-
-        for (key in mzr.breakpoints) {
-            if (key == "current") break;
-            if (nowWindowWidth <= mzr.breakpoints[key]) return key;
-            latest = key; 
-        }
-
-        return latest;
-    }
-
+window.addEventListener('resize', function () {
+  getCurrentBreakpoint();
 });
